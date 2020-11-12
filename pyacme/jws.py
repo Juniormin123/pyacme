5cbb93168d8eced8be00d6d1110924ac034d2c9b
Juniormin123 <min641366609@live.com>
2020-11-12 15:55:24 +0800

    kid handle for JWSRS256

--- pyacme/jws.py
+++ pyacme/jws.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 import base64
 
 import rsa
@@ -6,22 +6,29 @@ import rsa
 from pyacme.base import _JWSBase
 from pyacme.jwk import JWKRSA
 
+# TODO use a generic cryptography library
+
 
 class JWSRS256(_JWSBase):
     
     alg = 'RS256'
     hash_method = 'SHA-256'
     
-    def __init__(self, url: str, nonce: str, 
-                 jwk: JWKRSA, payload: Dict[str, Any]):
-        self.jwk: JWKRSA
+    def __init__(self, 
+                 url: str, 
+                 nonce: str, 
+                 jwk: JWKRSA, 
+                 kid: str = '', 
+                 payload: Dict[str, Any] = dict()):
+        # self.jwk: JWKRSA
         if not isinstance(jwk, JWKRSA):
             raise TypeError(
                 f'jwk type "{type(jwk)}" not compatible with {self.alg}'
             )
-        super().__init__(self.alg, url, nonce, jwk, payload)
+        super().__init__(self.alg, url, nonce, payload, jwk, kid)
     
     def sign(self) -> None:
+        self.jwk: JWKRSA
         sign_input = self.get_sign_input()
         sig = rsa.sign(sign_input, self.jwk.priv_key, self.hash_method)
         self.signature = str(
