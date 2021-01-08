67931b6969edae113ed19f9b24b7f5d2b9210c49
calvin <calvin.cheng@spiral.exchange>
2021-01-08 18:23:48 +0800

    test for multi domains

--- test/integration_test.py
+++ test/integration_test.py
@@ -77,7 +77,7 @@ def _common(self, params: dict, ca = 'pebble'):
         wd = Path(params['working_directory']).expanduser().absolute()
     else:
         wd = Path(WD_DEFAULT).expanduser().absolute()
-    wd = wd / '_'.join(self.domain)
+    wd = wd / '_'.join(params['domain'])    # domain must exist
     root_cert = 'pebble-root-cert.pem'
     if ca == 'pebble':
         download_root_cert(wd / WD_CERT)
@@ -100,10 +100,25 @@ class IntegrationHttpMode(unittest.TestCase):
         run_pebble_standalone_container()
         # add to host file manually if sudo is not intended
         self.domain = ['test-integration.local']
+        self.multi_domain = [
+            'a.test-integration.local', 'b.test-integration.local'
+        ]
     
     def test_http_run(self):
         params = dict(
             domain=self.domain,
+            contact=TEST_CONTACTS,
+            country_code='UN',
+            mode='http',
+            CA_entry=PEBBLE_TEST,
+            no_ssl_verify=True,
+            chall_resp_server_port=PY_HTTPSERVER_PORT
+        )
+        _common(self, params)
+
+    def test_http_multi_domain(self):
+        params = dict(
+            domain=self.multi_domain,
             contact=TEST_CONTACTS,
             country_code='UN',
             mode='http',
@@ -151,6 +166,10 @@ class IntegrationDNSModeStaging(unittest.TestCase):
         # run_pebble_standalone_container()
         self.domain = ['test-staging.xn--jhqy4a5a064kimjf01df8e.host']
         self.wild_card_domain = ['*.xn--jhqy4a5a064kimjf01df8e.host']
+        self.multi_domain = [
+            'test-staging-1.xn--jhqy4a5a064kimjf01df8e.host',
+            'test-staging-2.xn--jhqy4a5a064kimjf01df8e.host',
+        ]
         # self.domain = ['xn--jhqy4a5a064kimjf01df8e.host']
         self.aliyun_ak = get_aliyun_access_key('test/.aliyun_dns_api.json')
     
@@ -173,5 +192,17 @@ class IntegrationDNSModeStaging(unittest.TestCase):
             CA_entry=LETSENCRYPT_STAGING,
             access_key=self.aliyun_ak['access_key'],
             secret=self.aliyun_ak['secret']
+        )
+        _common(self, params, ca='staging')
+
+    def test_dns_mutlti_domain(self):
+        params = dict(
+            domain=self.multi_domain,
+            contact=TEST_CONTACTS,
+            country_code='UN',
+            CA_entry=LETSENCRYPT_STAGING,
+            access_key=self.aliyun_ak['access_key'],
+            secret=self.aliyun_ak['secret'],
+            debug=True
         )
         _common(self, params, ca='staging')
